82375dad0ff460c77818a1e387285be6d8589655
Michael <michael.zhai@centrality.ai>
2019-08-06 15:28:15 +1200

    Init function examples

---
+++ function/src/main.rs
@@ -0,0 +1,26 @@
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+    lhs % rhs == 0
+}
+
+fn fizzbuzz(n: u32) {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+fn fizzbuzz_to(n: u32) {
+    for m in 1..n+1 {
+        fizzbuzz(m);
+    }
+}
+
+fn main() {
+    fizzbuzz_to(100);
+}
